c88b73d420adf0aac618d6439f5d26fae8850a01
sfidahussain <sanfidahussain@gmail.com>
2017-11-22 11:41:59 -0600

    Added most of initial files

---
+++ js/data.js
@@ -0,0 +1,747 @@
+var data = [
+    {
+        name: "PUT A SMILE ON SS Graphic Tee",
+        description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTENER WASH W/<br>WATERBASE PRINT",
+        style: [
+            {
+                num: "32",
+                bg: "dg"
+            },
+            {
+                num: "33",
+                bg: "lg"
+            },
+            {
+                num: "34",
+                bg: "dg"
+            },
+            {
+                num: "41",
+                bg: "dg"
+            }
+        ],
+        pathway: "PUT A SMILE ON/"
+    },
+     {
+         name: "GET REAL SS Graphic Tee",
+         description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTENER WASH<br>W/ DISCHARGE PRINT",
+         style: [
+            {
+                num: "29",
+                bg: "lg"
+            },
+             {
+                 num: "30",
+                 bg: "lg"
+             },
+             {
+                 num: "31",
+                 bg: "lg"
+             }
+         ],
+         pathway: "GET REAL/"
+     },
+     {
+         name: "LET FREEDOM SOAR SS Graphic Tee",
+         description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTNER WASH W/<br>WATERBASE PRINT",
+         style: [
+            {
+                num: "35",
+                bg: "lg"
+            },
+             {
+                 num: "36",
+                 bg: "dg"
+             },
+             {
+                 num: "37",
+                 bg: "lg"
+             }
+         ],
+         pathway: "LET FREEDOM SOAR/"
+     },
+     {
+         name: "PEACE BOMB SS Graphic Tee",
+         description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTENER WASH W/<br>DISCHARGE PRINT",
+         style: [
+             {
+                 num: "38",
+                 bg: "lg"
+             },
+             {
+                 num: "39",
+                 bg: "dg"
+             },
+             {
+                 num: "40",
+                 bg: "dg"
+             }
+         ],
+         pathway: "PEACE BOMB/"
+     },
+     {
+         name: "ARMED WITH IDEAS SS Graphic Tee Option 2",
+         description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTNER WASH W/<br>FLOCKED CHEST PRINT<br>CAMO ALL OVER PIGMENT PRINT",
+         style: [
+            {
+                num: "17",
+                bg: "lg"
+            },
+             {
+                 num: "19",
+                 bg: "lg"
+             }
+         ],
+         pathway: "ARMED WITH IDEAS/"
+     },
+     {
+         name: "ARMED WITH IDEAS SS Graphic Tee Option 3",
+         description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTNER WASH W/<br>FLOCKED CHEST PRINT<br>CAMO ALL OVER PIGMENT PRINT<br>camo",
+         style: [
+             {
+                 num: "19",
+                 bg: "lg"
+             },
+             {
+                 num: "17",
+                 bg: "lg"
+             }
+         ],
+         pathway: "ARMED WITH IDEAS/"
+     },
+     {
+         name: "DIVIDED SS Graphic Tee",
+         description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTNER WASH<br>SOLID: REACTIVE DYE<br>CAMO: PIGMENT PRINT<br>HIGHLINE: DISCHARGE",
+         style: [
+            {
+                num: "18",
+                bg: "lg"
+            },
+             {
+                 num: "19",
+                 bg: "lg"
+             }
+         ],
+         pathway: "DIVIDED/"
+     },
+     {
+         name: "KEEP ON TRUCKIN' LS Military Crewneck",
+         description: "100% COTTON RIB INTERLOCK<br>Y/D JACQUARD<br>250 GSM<br>LIGHTWEIGHT PEACHED CANVAS ON<br>SHOULDERS, POCKET, AND ARMS<br>FLAP CHEST POCKET<br>HIDDEN ZIP HAND POCKETS",
+         style: [
+            {
+                num: "9",
+                bg: "lg"
+            },
+             {
+                 num: "10",
+                 bg: "dg"
+             },
+             {
+                 num: "12",
+                 bg: "dg"
+             },
+             {
+                 num: "13",
+                 bg: "dg"
+             },
+             {
+                 num: "14",
+                 bg: "dg"
+             },
+             {
+                 num: "15",
+                 bg: "dg"
+             }
+         ],
+         pathway: "KEEP ON TRUCKIN/"
+     },
+     {
+         name: "KILLER DILLER LS Pocket Knit Shirt",
+         description: "60% COTTON 40% POLYESTER<br>270 GSM<br>TWILL POCKET",
+         style: [
+            {
+                num: "39",
+                bg: "lg"
+            },
+             {
+                 num: "40",
+                 bg: "lg"
+             },
+             {
+                 num: "44",
+                 bg: "lg"
+             }
+         ],
+         pathway: "KILLER DILLER/"
+     },
+     {
+         name: "ACE LS French Terry Notched V-Neck Pullover",
+         description: "85% POLYESTER, 15% COTTON<br>240 GSM",
+         style: [
+            {
+                num: "25",
+                bg: "lg"
+            },
+             {
+                 num: "26",
+                 bg: "lg"
+             },
+             {
+                 num: "27",
+                 bg: "lg"
+             }
+         ],
+         pathway: "ACE/"
+     },
+     {
+         name: "HIP TO THE GROVE LS Henley",
+         description: "215 GSM<br>DISCHARGE PRINT",
+         style: [
+            {
+                num: "25",
+                bg: "lg"
+            },
+             {
+                 num: "26",
+                 bg: "lg"
+             },
+             {
+                 num: "27",
+                 bg: "dg"
+             }
+         ],
+         pathway: "HIP TO THE GROOVE/"
+     },
+     {
+         name: "L8R LS Knit Shirt",
+         description: "COTTON/POLY<br>MILITARY THERMAL<br>CONTRAST BACK<br>YOKE: REVERSE SIDE<br>OF FABRIC<br>BICEP: TWILL RAW<br>EDGE APPLIQUE PATCH",
+         style: [
+            {
+                num: "25",
+                bg: "lg"
+            },
+             {
+                 num: "26",
+                 bg: "dg"
+             },
+             {
+                 num: "28",
+                 bg: "lg"
+             },
+             {
+                 num: "29",
+                 bg: "lg"
+             }
+         ],
+         pathway: "L8R/"
+     },
+     {
+         name: "CATCH YA ON THE FLIP SIDE Hoodie",
+         description: "Textured fabric",
+         style: [
+            {
+                num: "8",
+                bg: "lg"
+            }
+         ],
+         pathway: "CATCH YA ON THE FLIP SIDE/"
+     },
+     {
+         name: "BE HUMAN & KIND LS Sweater",
+         description: "92% POLYESTER, 8% SPANDEX<br>JACQUARD INTERIOR: 65%<br>POLYESTER, 35% RAYON JERSEY<br>475 GSM<br>DYED, BONDED W/ SPONGE BACK<br>SIDE PILLING",
+         style: [
+            {
+                num: "25",
+                bg: "lg"
+            },
+             {
+                 num: "27",
+                 bg: "lg"
+             },
+             {
+                 num: "28",
+                 bg: "lg"
+             }
+         ],
+         pathway: "BE HUMAN AND KIND/"
+     },
+     {
+         name: "BE HUMAN & KIND Jogger",
+         description: "92% POLYESTER, 8% SPANDEX<br>JACQUARD INTERIOR: 65%<br>POLYESTER, 35% RAYON JERSEY<br>475 GSM<br>DYED, BONDED W/ SPONGE BACK<br>SIDE PILLING",
+         style: [
+             {
+                 num: "25",
+                 bg: "dg"
+             },
+             {
+                 num: "27",
+                 bg: "lg"
+             },
+             {
+                 num: "28",
+                 bg: "lg"
+             }
+         ],
+         pathway: "BE HUMAN AND KIND/"
+     },
+     {
+         name: "RECON Sweater",
+         description: "60% Cotton 40% Acrylic",
+         style: [
+            {
+                num: "42",
+                bg: "lg"
+            }
+         ],
+         pathway: "RECON/"
+     },
+     {
+         name: "OFF THE HOOK Full Coat Garment Dyed Pique SS",
+         description: "100% COTTON<br>WAFFLE WEAVE PIQUE<br>GARMENT DYE",
+         style: [
+            {
+                num: "24",
+                bg: "lg"
+            },
+             {
+                 num: "26",
+                 bg: "lg"
+             },
+             {
+                 num: "27",
+                 bg: "lg"
+             },
+             {
+                 num: "28",
+                 bg: "lg"
+             },
+             {
+                 num: "29",
+                 bg: "lg"
+             },
+             {
+                 num: "30",
+                 bg: "lg"
+             }
+         ],
+         pathway: "OFF THE HOOK SS/"
+     },
+     {
+         name: "OFF THE HOOK Full Coat Garment Dyed Pique LS",
+         description: "100% COTTON<br>WAFFLE WEAVE PIQUE<br>GARMENT DYE",
+         style: [
+            {
+                num: "24",
+                bg: "lg"
+            },
+             {
+                 num: "26",
+                 bg: "lg"
+             },
+             {
+                 num: "27",
+                 bg: "lg"
+             },
+             {
+                 num: "28",
+                 bg: "lg"
+             },
+             {
+                 num: "29",
+                 bg: "lg"
+             },
+             {
+                 num: "30",
+                 bg: "lg"
+             }
+         ],
+         pathway: "OFF THE HOOK LS/"
+     },
+     {
+         name: "CATCH MY DRIFT Woven Popover Shirt",
+         description: "BICEP PATCH<br>Dual flap pockets<br>Epaulets",
+         style: [
+            {
+                num: "47",
+                bg: "lg"
+            },
+             {
+                 num: "48",
+                 bg: "lg"
+             },
+             {
+                 num: "50",
+                 bg: "lg"
+             }
+         ],
+         pathway: "CATCH MY DRIFT/"
+     },
+     // {
+     //     name: "ROAMING BUFFALO Shirt",
+     //     description: "100% COTTON<br>Dual flap pockets",
+     //     style: [
+     //         {
+     //             num: "55",
+     //             bg: "lg"
+     //         },
+     //         {
+     //             num: "56",
+     //             bg: "lg"
+     //         },
+     //         {
+     //             num: "57",
+     //             bg: "lg"
+     //         }
+     //     ],
+     //     pathway: "ROAMING BUFFALO/"
+     // },
+     {
+         name: "BLAZIN' Denim Jacket",
+         description: "99% COTTON, 1% SPANDEX<br>10 OZ.",
+         style: [
+            {
+                num: "37",
+                bg: "lg"
+            }
+         ],
+         pathway: "BLAZIN/"
+     },
+     {
+         name: "GNARLY Solid Jacket",
+         description: "80% POLYESTER, 20% NYLON<br>DULL FINISH<br>88 GSM<br>POLYFILL",
+         style: [
+             {
+                 num: "8",
+                 bg: "lg"
+             },
+             {
+                 num: "9",
+                 bg: "lg"
+             },
+             {
+                 num: "10",
+                 bg: "lg"
+             }
+         ],
+         pathway: "GNARLY/"
+     },
+     {
+         name: "GNARLY Camo Jacket",
+         description: "80% POLYESTER, 20% NYLON<br>DULL FINISH<br>88 GSM<br>POLYFIL",
+         style: [
+            {
+                num: "5",
+                bg: "lg"
+            },
+             {
+                 num: "7",
+                 bg: "lg"
+             }
+         ],
+         pathway: "GNARLY/"
+     },
+     {
+         name: "DY-NO-MITE! M1A Jacket Solid",
+         description: "POLYESTER TWILL",
+         style: [
+            {
+                num: "34",
+                bg: "lg"
+            },
+             {
+                 num: "35",
+                 bg: "lg"
+             },
+             {
+                 num: "36",
+                 bg: "lg"
+             }
+         ],
+         pathway: "DY-NO-MITE/"
+     },
+     {
+         name: "DY-NO-MITE! M1A Jacket Camo",
+         description: "POLYESTER TWILL",
+         style: [
+            {
+                num: "37",
+                bg: "lg"
+            },
+             {
+                 num: "38",
+                 bg: "lg"
+             }
+         ],
+         pathway: "DY-NO-MITE/"
+     },
+     {
+         name: "DY-NO-MITE! Jogger Solid",
+         description: "POLYESTER TWILL",
+         style: [
+            {
+                num: "7",
+                bg: "lg"
+            },
+             {
+                 num: "8",
+                 bg: "lg"
+             },
+             {
+                 num: "9",
+                 bg: "lg"
+             }
+         ],
+         pathway: "DY-NO-MITE JOGGER/"
+     },
+     {
+         name: "DY-NO-MITE! Jogger Camo",
+         description: "POLYESTER TWILL",
+         style: [
+            {
+                num: "10",
+                bg: "lg"
+            },
+             {
+                 num: "11",
+                 bg: "lg"
+             }
+         ],
+         pathway: "DY-NO-MITE JOGGER/"
+     },
+    {
+        name: "FLASH BACK Military Jacket",
+        description: "100% Cotton Slub<br>Canvas",
+        style: [
+           {
+               num: "4",
+               bg: "lg"
+           },
+            {
+                num: "7",
+                bg: "lg"
+            },
+            {
+                num: "8",
+                bg: "lg"
+            }
+        ],
+        pathway: "FLASH BACK/"
+    },
+     {
+         name: "Roger That M65 Jacket",
+         description: "Polyster Twill",
+         style: [
+            {
+                num: "8",
+                bg: "dg"
+            },
+             {
+                 num: "9",
+                 bg: "lg"
+             },
+             {
+                 num: "9",
+                 bg: "lg"
+             }
+         ],
+         pathway: "M65 JACKET/"
+     },
+     {
+         name: "PEACE GRENADE Mock Neck Jacket Solid",
+         description: "100% POLYESTER<br>BICEP PATCHES<br>WEARER\'S LEFT CHEST PRINT",
+         style: [
+            {
+                num: "7",
+                bg: "lg"
+            },
+             {
+                 num: "8",
+                 bg: "lg"
+             },
+             {
+                 num: "9",
+                 bg: "lg"
+             }
+         ],
+         pathway: "PEACE GRENADE/"
+     },
+     {
+         name: "PEACE GRENADE Mock Neck Jacket Camo",
+         description: "100% POLYESTER<br>BICEP PATCHES<br>WEARER\'S LEFT CHEST PRINT",
+         style: [
+            {
+                num: "5",
+                bg: "lg"
+            },
+             {
+                 num: "6",
+                 bg: "lg"
+             }
+         ],
+         pathway: "PEACE GRENADE/"
+     },
+     {
+         name: "KILLER Denim Laser Etched Camo Jeans",
+         description: "99% COTTON, 1% SPANDEX<br>10 OZ",
+         style: [
+            {
+                num: "1",
+                bg: "lg"
+            }
+         ],
+         pathway: "KILLER/"
+     },
+     {
+         name: "TUBULAR Plaid Cargo Pants",
+         description: "POLY/RAYON/SPANDEX<br>STRETCH FLANNEL<br>ZIP CARGO POCKETS<br>PHONE POCKET<br>ZIPPERS AT LEG OPENINGS",
+         style: [
+            {
+                num: "13",
+                bg: "lg"
+            },
+             {
+                 num: "15",
+                 bg: "lg"
+             }
+         ],
+         pathway: "TUBULAR/"
+     },
+     {
+         name: "LE CHASSEUR Pant",
+         description: "98% COTTON 2% SPANDEX<br>250 GSM<br>FLAP POCKETS<br>ZIP HIP POCKETS<br>ZIP AT LEG OPENINGS",
+         style: [
+            {
+                num: "4",
+                bg: "lg"
+            },
+             {
+                 num: "5",
+                 bg: "lg"
+             },
+             {
+                 num: "6",
+                 bg: "lg"
+             }
+         ],
+         pathway: "LE CHASSEUR/"
+     },
+     {
+         name: "GUNG HO Jogger",
+         description: "94% POLYESTER 6% SPANDEX<br>DULL FINISH",
+         style: [
+            {
+                num: "0",
+                bg: "lg"
+            },
+             {
+                 num: "1",
+                 bg: "lg"
+             },
+             {
+                 num: "2",
+                 bg: "lg"
+             }
+         ],
+         pathway: "GUNG HO/"
+     },
+     {
+         name: "DAZE T Shirt",
+         description: "160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTENER WASH<br>GARMENT DYED",
+         style: [
+            {
+                num: "7",
+                bg: "lg"
+            },
+             {
+                 num: "8",
+                 bg: "dg"
+             },
+             {
+                 num: "9",
+                 bg: "dg"
+             }
+         ],
+         pathway: "DAZE/"
+     },
+     {
+         name: "COPASETIC LS French Terry Pullover",
+         description: "100% COTTON<br>160 GSM<br>COMBED & COMPACTED<br>GARMENT SOFTENER WASH",
+         style: [
+             {
+                 num: "29",
+                 bg: "lg"
+             },
+             {
+                 num: "30",
+                 bg: "lg"
+             },
+             {
+                 num: "31",
+                 bg: "lg"
+             },
+             {
+                 num: "32",
+                 bg: "lg"
+             },
+             {
+                 num: "33",
+                 bg: "lg"
+             }
+         ],
+         pathway: "COPASETIC/"
+     },
+     {
+         name: "HANG LOOSE Track Jacket",
+         description: "100% POLYESTER<br>TEXTURED TERRY<br>250 GSM<br>WELT HAND POCKETS",
+         style: [
+             {
+                 num: "5",
+                 bg: "lg"
+             },
+             {
+                 num: "6",
+                 bg: "lg"
+             },
+             {
+                 num: "7",
+                 bg: "lg"
+             },
+             {
+                 num: "8",
+                 bg: "lg"
+             },
+             {
+                 num: "9",
+                 bg: "lg"
+             }
+         ],
+         pathway: "HANG LOOSE/"
+     },
+     {
+         name: "HANG LOOSE Track Pant",
+         description: "100% POLYESTER<br>TEXTURED TERRY<br>250 GSM",
+         style: [
+             {
+                 num: "7",
+                 bg: "lg"
+             },
+             {
+                 num: "8",
+                 bg: "lg"
+             },
+             {
+                 num: "9",
+                 bg: "lg"
+             },
+             {
+                 num: "10",
+                 bg: "lg"
+             },
+             {
+                 num: "11",
+                 bg: "lg"
+             }
+         ],
+         pathway: "HANG LOOSE TRACK PANT/"
+     }
+];
